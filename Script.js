aa7a31195e20b16dbb699955c07791dbfaf84276
Mahdi <mahdiesmaili400@gmail.com>
2025-03-22 14:34:08 +0330

    Update Script.js

--- Script.js
+++ Script.js
@@ -15,8 +15,3 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 </script>
-<head>
-<body>
-    <!-- اسکریپت Lightbox از CDN -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.3/js/lightbox.min.js"></script>
-</body>
