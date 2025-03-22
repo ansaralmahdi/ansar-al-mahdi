1b9b6448fd651aaef6a37ae76a22616fa0af00d3
Mahdi <mahdiesmaili400@gmail.com>
2025-03-23 00:18:40 +0330

    Update Script.js

--- Script.js
+++ Script.js
@@ -51,3 +51,11 @@ function toggleMenu() {
     var menu = document.getElementById("menu");
     menu.classList.toggle("show");
 <script>
+<script>
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("menu-toggle").addEventListener("click", function () {
+        let menu = document.getElementById("menu");
+        menu.classList.toggle("hidden");
+    });
+});
+</script>
