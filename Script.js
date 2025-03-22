b2a74b6877130f44db47c536ddf782259ce8273b
Mahdi <mahdiesmaili400@gmail.com>
2025-03-22 12:54:55 +0330

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     alert("به سایت من خوش آمدید!");
 });
+<script>
+    const swiper = new Swiper('.swiper-container', {
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            640: { slidesPerView: 1 },
+            768: { slidesPerView: 2 },
+            1024: { slidesPerView: 3 }
+        }
+    });
+</script>
